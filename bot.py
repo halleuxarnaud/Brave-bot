dda972c2330baaa54cac049ae97ae0308f744bef
Buubulle <halleuxarnaud4@gmail.com>
2022-11-15 17:01:10 +0100

    Update

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ while(1):
     i = 0
     k = 0
     while(i <= 10):
-        os.startfile('C:/Program Files/BraveSoftware/Brave-Browser/Application')
+        os.startfile('C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe')
         time.sleep(5)
         while(k <= 10):
             pyautogui.hotkey('ctrl', 'r')
@@ -15,4 +15,7 @@ while(1):
         k = 0
         pyautogui.hotkey('alt', 'f4')
         i = i + 1
+    i = 0
+    os.startfile('C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe')
     time.sleep(3100)
+    pyautogui.hotkey('alt', 'f4')
