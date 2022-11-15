172a56d48942b65d3a93fcfbc67d1dcc8ed3521a
Buubulle <halleuxarnaud4@gmail.com>
2022-11-15 09:31:37 +0100

    Finish

---
+++ bot.py
@@ -0,0 +1,18 @@
+import pyautogui
+import time
+import os
+
+while(1):
+    i = 0
+    k = 0
+    while(i <= 10):
+        os.startfile('C:/Program Files/BraveSoftware/Brave-Browser/Application')
+        time.sleep(5)
+        while(k <= 10):
+            pyautogui.hotkey('ctrl', 'r')
+            time.sleep(5)
+            k = k + 1
+        k = 0
+        pyautogui.hotkey('alt', 'f4')
+        i = i + 1
+    time.sleep(3100)
